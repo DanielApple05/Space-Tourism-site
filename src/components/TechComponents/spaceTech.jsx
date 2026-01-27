b84a916ef04616fbd7ee8518620e1965b49254d7
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-01-27 18:55:06 +0100

    design done

--- src/components/TechComponents/spaceTech.jsx
+++ src/components/TechComponents/spaceTech.jsx
@@ -4,24 +4,25 @@ import { useState } from 'react';
 const spaceTech = ({ spaceLaunch }) => {
   const spaceData = spaceLaunch.technologyDetails.technologyDetails;
   const [activeBtn, setActiveBtn] = useState(0);
-  const techTab = {  btn: ["1", "2", "3"]
+  const techTab = {
+    btn: ["1", "2", "3"]
   };
-  console.log("spaceData", spaceData )
+  console.log("spaceData", spaceData)
 
   return (
 
-    <div className='text-white mx-[165px] my-[48px] w-[100%]'>
+    <div className='text-white ml-[165px] my-[48px] '>
       <h1 className='text-white text-[28px] pb-[24px]'>  <span className='text-[#979797]'>03</span> SPACE LAUNCH 101</h1>
 
-      <div>
+      <div className='flex w-[100%]'>
 
-        <div>
+        <div className='flex w-[50%] space-x-[64px] mr-[32px] my-[215px]'>
           <div className="grid gap-[32px]">
             {techTab.btn.map((btn, index) => (
               <button
                 key={index}
                 onClick={() => setActiveBtn(index)}
-                className={`py-[21px] px-[32px] border rounded-full cursor-pointer
+                className={`py-[21px] px-[35px] border rounded-full cursor-pointer text-[32px]
             ${activeBtn === index ? "bg-white text-black" : "bg-transparent    text-white"}
                `}
               >
@@ -30,17 +31,25 @@ const spaceTech = ({ spaceLaunch }) => {
             ))}
           </div>
 
-
-          <div>
-            <h4>
+          <div className='space-y-[20px] grid content-between '>
+            <h4 className='text-[32px] text-[#979797]'>
               THE TERMINOLOGY
             </h4>
-            <h2>
+            <h2 className='text-[56px]'>
               {spaceData[activeBtn].name}
             </h2>
+            <p>
+              {
+                spaceData[activeBtn].description
+              }
+            </p>
           </div>
 
 
+        </div>
+
+        <div className='w-[50%] py-[67px]  '>
+          <img src={spaceData[activeBtn].images.portrait} alt="" className='h-[600px] w-[622px] ' />
         </div>
 
       </div>
